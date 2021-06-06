b76f645dbe879a21de3458166178c53ae9459b36
Paramjeet7183 <62829715+Paramjeet7183@users.noreply.github.com>
2021-06-06 14:11:17 +0530

    Add files via upload

---
+++ build/precache-manifest.b0a9c55ed546f6d06d37e066338a96ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59fd38fd42b11f9b88bd075aa0eee998",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8f0ac97c175515cd07b",
+    "url": "/static/css/2.38ca0958.chunk.css"
+  },
+  {
+    "revision": "d5691e849ad29ed712af",
+    "url": "/static/css/main.2dd75947.chunk.css"
+  },
+  {
+    "revision": "c8f0ac97c175515cd07b",
+    "url": "/static/js/2.20dcbc50.chunk.js"
+  },
+  {
+    "revision": "2eec0a263ef1f7393322e8c89c5ff82a",
+    "url": "/static/js/2.20dcbc50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5691e849ad29ed712af",
+    "url": "/static/js/main.dd9c0b37.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
